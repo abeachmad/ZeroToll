b3a2764d17ced79d5fa37f7b9d01961ba637073e
abeachmad <abeachmad@users.noreply.github.com>
2026-03-01 21:45:23 +0800

    Restore to commit 91c43471: Add EIP-7702 MetaMask documentation

--- packages/contracts/contracts/interfaces/IPriceOracle.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-interface IPriceOracle {
-    function getPrice() external view returns (uint256);
-    function lastUpdated() external view returns (uint256);
-}
